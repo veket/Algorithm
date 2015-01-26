c5beca166e1696b3f1c986dc093ef9b54e050231
veket <veket201791@126.com>
2015-01-26 20:28:01 +0800

    Create ListNode.java

---
+++ ListNode.java
@@ -0,0 +1,27 @@
+/*---------------------------------------------------------------------------*/
+
+/**
+
+ * @(#)$Id: ListNode.java 9561 19 Jan 2015 19:46:10 $
+
+ * @(#) Implementation file of class ListNode.
+
+ * @author SearchNLP
+
+ *  (c)  SUNTEC CORPORATION  2015
+
+ * All Rights Reserved.
+
+ */
+
+/*---------------------------------------------------------------------------*/
+package net.suntec.cooljunit;
+
+public class ListNode {
+	int val;
+	ListNode next;
+	ListNode(int x){
+		val = x;
+		next = null;
+	}
+}
