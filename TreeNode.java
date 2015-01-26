f438b9e9aba49148e7e8ed0f2935d2861957d0d5
veket <veket201791@126.com>
2015-01-26 20:31:55 +0800

    Create TreeNode.java

---
+++ TreeNode.java
@@ -0,0 +1,29 @@
+/*---------------------------------------------------------------------------*/
+
+/**
+
+ * @(#)$Id: TreeNode.java 9561 19 Jan 2015 16:42:02 $
+
+ * @(#) Implementation file of class TreeNode.
+
+ * @author SearchNLP
+
+ *  (c)  SUNTEC CORPORATION  2015
+
+ * All Rights Reserved.
+
+ */
+
+/*---------------------------------------------------------------------------*/
+package net.suntec.cooljunit;
+
+public class TreeNode {
+	int val;
+	TreeNode left;
+	TreeNode right;
+
+	TreeNode(int x) {
+		val = x;
+	}
+
+}
